1816d4560312357746c0f5790dd2d670a320df1b
ToomGomes <thomasvirgilio@hotmail.com>
2021-11-24 16:16:12 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/valdir/helpdesk/domain/Tecnico.java
@@ -0,0 +1,10 @@
+package com.valdir.helpdesk.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tecnico extends Pessoa{
+
+	private List<Chamado> chamados = new ArrayList<>();
+	
+}
