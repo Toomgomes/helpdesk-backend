aa9658abf900c4704036af85f8371f4e8dccac4f
Toomgomes <94655087+Toomgomes@users.noreply.github.com>
2022-03-11 17:47:39 -0300

    create para chamado

--- src/main/java/com/valdir/helpdesk/services/ChamadoService.java
+++ src/main/java/com/valdir/helpdesk/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.valdir.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.valdir.helpdesk.domain.Chamado;
+import com.valdir.helpdesk.domain.Cliente;
+import com.valdir.helpdesk.domain.Tecnico;
+import com.valdir.helpdesk.domain.dtos.ChamadoDTO;
+import com.valdir.helpdesk.domain.enums.Prioridade;
+import com.valdir.helpdesk.domain.enums.Status;
 import com.valdir.helpdesk.repositories.ChamadoRepository;
 import com.valdir.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -15,6 +22,11 @@ public class ChamadoService {
 
 	@Autowired
 	private ChamadoRepository repository;
+	@Autowired
+	private TecnicoService tecnicoservice;
+	@Autowired
+	private ClienteService clienteservice;
+	
 	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
@@ -23,5 +35,26 @@ public class ChamadoService {
 
 	public List<Chamado> findAll() {
 		return repository.findAll();
+	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoservice.findById(obj.getTecnico());
+		Cliente cliente = clienteservice.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
 	}
 }
